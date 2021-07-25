0859527cccb8dabf8a1e02558274f461eef3a377
RebbD <kauavictor259@gmail.com>
2021-07-25 17:59:45 -0300

    sing up feito

--- login.php
+++ login.php
@@ -34,7 +34,32 @@
         <div class="separador"></div>
 
         <section class="login">
+            <h1>Sing Up</h1>
 
+            <form action="login.php" method="post">
+
+                <div class="campos">
+                    <label for="nome" class="labellogin">Nome</label>
+                    <input type="text" name="nome" id="nome" class="inputs">
+                </div>
+                <div class="campos">
+                    <label for="email" class="labellogin">Email</label>
+                    <input type="email" name="email" id="email" class="inputs">
+                </div>
+                <div class="campos">
+                    <label for="loginSing" class="labellogin">Login</label>
+                    <input type="text" name="loginSing" id="loginSing" class="inputs">
+                </div>
+                <div class="campos">
+                    <label for="senhaSing" class="labellogin">Senha</label>
+                    <input type="password" name="senhaSing" id="senhaSing" class="inputs">
+                </div>
+                <div class="campos">
+                    <label for="RsenhaSing" class="labellogin">Repita a Senha</label>
+                    <input type="password" name="RsenhaSing" id="RsenhaSing" class="inputs">
+                </div>
+                <button type="submit" id="btnSing">Sing Up</button>
+            </form>
         </section>
     </main>
 
